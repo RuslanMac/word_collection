8b23ac7ff03ec2813b9a0bda471b7ffe18d685f3
Ruslan <ruslan-karamurzanov@mail.ru>
2020-09-23 12:44:48 +0700

    first commit

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WORDS_PER_PAGE = 20
-    YANDEX_DICTIONARY_KEY = os.environ.get('YANDEX_DICTIONARY_KEY')
+    YANDEX_DICTIONARY_KEY = os.environ.get('YANDEX_DICTIONARY_KEY') or 'dict.1.1.20200813T170611Z.344edb4fae05f097.9f74cfd69db39bcd1d50fccb83112af2ba5e6290'
     LANGUAGES = ['en','ru']
     NEWSAPI = NewsApiClient(api_key='3221932dd191465fa7332c86eb222a00')
